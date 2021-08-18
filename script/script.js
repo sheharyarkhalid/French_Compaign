99a04289e813f978b8e4fe3a7c9a577f6ebbfea7
Sheharyar Khalid <sheharyarkhalid0313@gmail.com>
2021-08-18 22:00:05 +0500

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,161 @@
+
+function login_section(){
+    $("#login_section").fadeIn();
+   $("#login_section").css({"visibility":"visible","display":"block"});
+
+   $("#campaign_progress").fadeOut();
+   $("#campaign_progress").css({"visibility":"hidden","display":"none"});
+
+   $("#create_campaign_page_one").fadeOut();
+   $("#create_campaign_page_one").css({"visibility":"hidden","display":"none"});
+
+   $("#create_campaign_page_two").fadeOut();
+   $("#create_campaign_page_two").css({"visibility":"hidden","display":"none"});
+
+   $("#create_campaign_page_three").fadeOut();
+   $("#create_campaign_page_three").css({"visibility":"hidden","display":"none"});
+
+   $("#create_campaign_page_four").fadeOut();
+   $("#create_campaign_page_four").css({"visibility":"hidden","display":"none"});
+
+   $("#marketing_page").fadeOut();
+   $("#marketing_page").css({"visibility":"hidden","display":"none"});
+  }
+
+  function compain_progress_section(){
+    $("#login_section").fadeOut();
+   $("#login_section").css({"visibility":"hidden","display":"none"});
+
+   $("#campaign_progress").fadeIn();
+   $("#campaign_progress").css({"visibility":"visible","display":"block"});
+
+   $("#create_campaign_page_one").fadeOut();
+   $("#create_campaign_page_one").css({"visibility":"hidden","display":"none"});
+
+   $("#create_campaign_page_two").fadeOut();
+   $("#create_campaign_page_two").css({"visibility":"hidden","display":"none"});
+
+   $("#create_campaign_page_three").fadeOut();
+   $("#create_campaign_page_three").css({"visibility":"hidden","display":"none"});
+
+   $("#create_campaign_page_four").fadeOut();
+   $("#create_campaign_page_four").css({"visibility":"hidden","display":"none"});
+
+   $("#marketing_page").fadeOut();
+   $("#marketing_page").css({"visibility":"hidden","display":"none"});
+  }
+ 
+  function create_compain_section(){
+    $("#login_section").fadeOut();
+   $("#login_section").css({"visibility":"hidden","display":"none"});
+
+   $("#campaign_progress").fadeOut();
+   $("#campaign_progress").css({"visibility":"hidden","display":"none"});
+
+   $("#create_campaign_page_one").fadeIn();
+   $("#create_campaign_page_one").css({"visibility":"visible","display":"block"});
+
+   $("#create_campaign_page_two").fadeOut();
+   $("#create_campaign_page_two").css({"visibility":"hidden","display":"none"});
+
+   $("#create_campaign_page_three").fadeOut();
+   $("#create_campaign_page_three").css({"visibility":"hidden","display":"none"});
+
+   $("#create_campaign_page_four").fadeOut();
+   $("#create_campaign_page_four").css({"visibility":"hidden","display":"none"});
+
+   $("#marketing_page").fadeOut();
+   $("#marketing_page").css({"visibility":"hidden","display":"none"});
+  }
+
+  function create_campaign_page_two(){
+    $("#login_section").fadeOut();
+   $("#login_section").css({"visibility":"hidden","display":"none"});
+
+   $("#campaign_progress").fadeOut();
+   $("#campaign_progress").css({"visibility":"hidden","display":"none"});
+
+   $("#create_campaign_page_one").fadeOut();
+   $("#create_campaign_page_one").css({"visibility":"hidden","display":"none"});
+
+   $("#create_campaign_page_two").fadeIn();
+   $("#create_campaign_page_two").css({"visibility":"visible","display":"block"});
+
+   $("#create_campaign_page_three").fadeOut();
+   $("#create_campaign_page_three").css({"visibility":"hidden","display":"none"});
+
+   $("#create_campaign_page_four").fadeOut();
+   $("#create_campaign_page_four").css({"visibility":"hidden","display":"none"});
+
+   $("#marketing_page").fadeOut();
+   $("#marketing_page").css({"visibility":"hidden","display":"none"});
+  }
+
+  function create_campaign_page_three(){
+    $("#login_section").fadeOut();
+   $("#login_section").css({"visibility":"hidden","display":"none"});
+
+   $("#campaign_progress").fadeOut();
+   $("#campaign_progress").css({"visibility":"hidden","display":"none"});
+
+   $("#create_campaign_page_one").fadeOut();
+   $("#create_campaign_page_one").css({"visibility":"hidden","display":"none"});
+
+   $("#create_campaign_page_two").fadeOut();
+   $("#create_campaign_page_two").css({"visibility":"hidden","display":"none"});
+
+   $("#create_campaign_page_three").fadeIn();
+   $("#create_campaign_page_three").css({"visibility":"visible","display":"block"});
+
+   $("#create_campaign_page_four").fadeOut();
+   $("#create_campaign_page_four").css({"visibility":"hidden","display":"none"});
+
+   $("#marketing_page").fadeOut();
+   $("#marketing_page").css({"visibility":"hidden","display":"none"});
+  }
+
+  function create_campaign_page_four(){
+    $("#login_section").fadeOut();
+   $("#login_section").css({"visibility":"hidden","display":"none"});
+
+   $("#campaign_progress").fadeOut();
+   $("#campaign_progress").css({"visibility":"hidden","display":"none"});
+
+   $("#create_campaign_page_one").fadeOut();
+   $("#create_campaign_page_one").css({"visibility":"hidden","display":"none"});
+
+   $("#create_campaign_page_two").fadeOut();
+   $("#create_campaign_page_two").css({"visibility":"hidden","display":"none"});
+
+   $("#create_campaign_page_three").fadeOut();
+   $("#create_campaign_page_three").css({"visibility":"hidden","display":"none"});
+
+   $("#create_campaign_page_four").fadeIn();
+   $("#create_campaign_page_four").css({"visibility":"visible","display":"block"});
+
+   $("#marketing_page").fadeOut();
+   $("#marketing_page").css({"visibility":"hidden","display":"none"});
+  }
+
+  function marketing_section(){
+    $("#login_section").fadeOut();
+   $("#login_section").css({"visibility":"hidden","display":"none"});
+
+   $("#campaign_progress").fadeOut();
+   $("#campaign_progress").css({"visibility":"hidden","display":"none"});
+
+   $("#create_campaign_page_one").fadeOut();
+   $("#create_campaign_page_one").css({"visibility":"hidden","display":"none"});
+
+   $("#create_campaign_page_two").fadeOut();
+   $("#create_campaign_page_two").css({"visibility":"hidden","display":"none"});
+
+   $("#create_campaign_page_three").fadeOut();
+   $("#create_campaign_page_three").css({"visibility":"hidden","display":"none"});
+
+   $("#create_campaign_page_four").fadeOut();
+   $("#create_campaign_page_four").css({"visibility":"hidden","display":"none"});
+
+   $("#marketing_page").fadeIn();
+   $("#marketing_page").css({"visibility":"visible","display":"block"});
+  }
